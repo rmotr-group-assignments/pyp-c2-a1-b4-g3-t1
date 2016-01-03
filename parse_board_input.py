d5b98b893ef28734b3189dd7106c9567becd505b
Scott Munro <scottnmunro@gmail.com>
2016-01-03 15:42:37 -0600

    wrote unit tests for the parser and made the parse board input module return a dummy placeholder value

--- parse_board_input.py
+++ parse_board_input.py
@@ -1,4 +1,13 @@
 import re
+from ship import Ship
 
-def parse_board_builder_input(input):
-    return (None, None, None)
+def parse(input):
+    """Parse user's ship placement input string and return a triple containing
+    the parsed data
+
+    Raises: ValueError if input is invalid
+
+    Returns: a triple of the form (Shiptype, position, horizontal_orientation?)
+    """
+    # placeholder failing return value
+    return (Ship.Invalid, ('Z', -1), None)
--- parse_board_input_test.py
+++ parse_board_input_test.py
@@ -1,1 +1,42 @@
 import unittest
+from parse_board_input import parse as parse_bi
+from ship import Ship
+
+class TestParseBoardInput(unittest.TestCase):
+    def test_input_1(self):
+        expected = (Ship.Submarine, ('B', 2), True)
+        input = "Place subMarine at B2 horizontally"
+        self.assertEquals(parse_bi(input), expected)
+
+    def test_input_2(self):
+        expected = (Ship.Aircraft, ('E', 4), False)
+        input = "place aircraft at E4 vertically"
+        self.assertEquals(parse_bi(input), expected)
+
+    def test_input_3(self):
+        expected = (Ship.Patrol, ('C', 6), False)
+        input = "place patrol boat at C6 vertically"
+        self.assertEquals(parse_bi(input), expected)
+
+    def test_input_4(self):
+        expected = (Ship.Patrol, ('E', 3), True)
+        input = "place patrol boats at E3 horizontally"
+        self.assertEquals(parse_bi(input), expected)
+
+    def test_invalid_ship(self):
+        with self.assertRaises(ValueError):
+            input = "Place sandwich on C3 horizontally"
+            parse_bi(input)
+
+    def test_invalid_position(self):
+        with self.assertRaises(ValueError):
+            input = "Place patrol boat on Z3 vertically"
+            parse_bi(input)
+
+    def test_invalid_orientation(self):
+        with self.assertRaises(ValueError):
+            input = "Place submarine on d1 diagonally"
+            parse_bi(input)
+
+if __name__ == '__main__':
+    unittest.main()
