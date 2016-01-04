f3d2099ce132aadf10602327699e920932220139
Scott Munro <scottnmunro@gmail.com>
2016-01-03 20:24:50 -0600

    added the basic player interface for computer and human players

---
+++ player.py
@@ -0,0 +1,30 @@
+class Player(object):
+    def __init__(self):
+        self.ships_left = 4
+
+    def __str__(self):
+        raise NotImplementedError()
+
+    def choose_attack(self, defender):
+        """Choose a valid location to attack the defending player"""
+        raise NotImplementedError()
+
+    def receive_attack(self, attack_location):
+        """Register attack with this player's board and respond"""
+        raise NotImplementedError()
+
+    def update_enemy_board(self, attack_response):
+        """Update our board with the response from the enemy"""
+        raise NotImplementedError()
+
+    def has_lost(self):
+        """Returns whether this player's ships have all sunk"""
+        return self.ships_left == 0
+
+class HumanPlayer(Player):
+    # IMPLEMENT LATER
+    pass
+
+class ComputerPlayer(Player):
+    # IMPLEMENT LATER
+    pass
