54420084b740301e613dfb963c66a4ccb2919edc
Scott Munro <scottnmunro@gmail.com>
2016-01-04 17:39:34 -0600

    gave names to each of the placeholder characters for the enemy boards. i.e. miss marker, hit marker, and empty

--- board.py
+++ board.py
@@ -6,9 +6,11 @@ row_padding = string.ascii_uppercase[10:12]
 col_padding = list(range(10, 12))
 padded_row_range = row_range + row_padding
 padded_col_range = col_range + col_padding
-padding_sentinel = 0
 
-hitmarker = "H"
+padding_sentinel = 0
+hit_marker = "H"
+miss_marker = "M"
+empty_marker = " "
 
 
 class Board(object):
@@ -16,7 +18,7 @@ class Board(object):
         self.cells = {}
         for row in row_range:
             for col in col_range:
-                self.cells[(row, col)] = " "
+                self.cells[(row, col)] = empty_marker
         for row in row_padding:
             for col in col_padding:
                 self.cells[(row, col)] = padding_sentinel
@@ -28,10 +30,13 @@ class Board(object):
         self.cells[(row, col)] = ship
 
     def mark_hit(self, row, col):
-        self.cells[(row, col)] = hitmarker
+        self.write_cell(row, col, hit_marker)
+
+    def mark_miss(self, row, col):
+        self.write_cell(row, col, miss_marker)
 
     def cell_empty(self, row, col):
-        return self.read_cell(row, col) == " "
+        return self.read_cell(row, col) == empty_marker
 
     def __str__(self):
         display = ("     0 1 2 3 4 5 6 7 8 9\n" +
