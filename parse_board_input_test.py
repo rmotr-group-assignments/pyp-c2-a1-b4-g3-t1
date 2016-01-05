6381c44367645d0879893c1993db2d54626a4786
Scott Munro <scottnmunro@gmail.com>
2016-01-05 01:12:44 -0600

    returning a ship spec triple now in parse board input so need to update tests

--- parse_board_input_test.py
+++ parse_board_input_test.py
@@ -5,22 +5,22 @@ from ship import Submarine, PatrolBoat, Aircraft
 
 class TestParseBoardInput(unittest.TestCase):
     def test_input_1(self):
-        expected = Submarine(('B', 2), True)
+        expected = (Submarine, ('B', 2), True)
         input = "Place subMarine at B2 horizontally"
         self.assertEquals(parse_bi(input), expected)
 
     def test_input_2(self):
-        expected = Aircraft(('E', 4), False)
+        expected = (Aircraft, ('E', 4), False)
         input = "place aircraft at E4 vertically"
         self.assertEquals(parse_bi(input), expected)
 
     def test_input_3(self):
-        expected = PatrolBoat(('C', 6), False)
+        expected = (PatrolBoat, ('C', 6), False)
         input = "place patrol boat at C6 vertically"
         self.assertEquals(parse_bi(input), expected)
 
     def test_input_4(self):
-        expected = PatrolBoat(('E', 3), True)
+        expected = (PatrolBoat, ('E', 3), True)
         input = "place patrol boats at E3 horizontally"
         self.assertEquals(parse_bi(input), expected)
 
